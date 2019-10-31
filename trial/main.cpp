e88a7863e42a312ea139f87e9d0412e8530813e3
Gavin Kaepernick <kaepernick.gavin6@gmail.com>
2019-10-31 10:34:10 -0500

    Initial Commit

---
+++ trial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  trial
+//
+//  Created by Gavin Kaepernick on 10/31/19.
+//  Copyright © 2019 Gavin Kaepernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
